a94c92a7a0ed70a8776b431ffe0d60ec5c55234f
Mitchell Hudson <mitchellhudson@mitchells-MacBook-Pro.local>
2016-11-30 12:28:55 -0800

    Initial Commit

---
+++ StackView-Code/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StackView-Code
+//
+//  Created by mitchell hudson on 11/30/16.
+//  Copyright © 2016 Mitchell Hudson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
